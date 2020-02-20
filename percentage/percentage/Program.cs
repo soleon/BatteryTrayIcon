633dc51821cb14e55c001a15a75942447dd3997c
Leon <leon.zhou@smokeball.com>
2020-02-20 17:36:41 +1100

    Converted to .NET Core 3.1, removed unnecessary code and merged code to simplify structure, update syntax to C# 8, fixed blurry font, updated to use system default font, updated to transparency background, updated to work with Windows 10 dark and light theme, extended battery check interval to 10 seconds from 1.

--- percentage/percentage/Program.cs
+++ percentage/percentage/Program.cs
@@ -1,22 +1,69 @@
-﻿using System;
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
-namespace percentage
+namespace Percentage
 {
-    static class Program
+    internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+            using var notifyIcon = new NotifyIcon {Visible = true};
+            var item = new ToolStripMenuItem("Exit");
+            item.Click += (_, __) => Application.Exit();
+            notifyIcon.ContextMenuStrip = new ContextMenuStrip {Items = {item}};
 
-            TrayIcon trayIcon = new TrayIcon();
+            Update();
+
+            var timer = new Timer {Interval = 10000};
+            timer.Tick += (_, __) => Update();
+            timer.Start();
 
             Application.Run();
+
+            void Update()
+            {
+                var text = (SystemInformation.PowerStatus.BatteryLifePercent * 100)
+                    .ToString(CultureInfo.CurrentCulture);
+                SizeF textSize;
+                using (var bitmap = new Bitmap(1, 1))
+                {
+                    using var graphics = Graphics.FromImage(bitmap);
+                    textSize = graphics.MeasureString(text, SystemFonts.DefaultFont);
+                }
+
+                using (var bitmap = new Bitmap((int) textSize.Width, (int) textSize.Height))
+                {
+                    using (var graphics = Graphics.FromImage(bitmap))
+                    {
+                        graphics.DrawString(text, SystemFonts.DefaultFont,
+                            1.Equals(Registry.GetValue(
+                                @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize",
+                                "SystemUsesLightTheme", null))
+                                ? Brushes.Black
+                                : Brushes.White, 0, 0);
+                    }
+
+                    var handle = bitmap.GetHicon();
+                    try
+                    {
+                        notifyIcon.Icon?.Dispose();
+                        notifyIcon.Icon = Icon.FromHandle(handle);
+                    }
+                    finally
+                    {
+                        DestroyIcon(handle);
+                    }
+                }
+            }
         }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool DestroyIcon(IntPtr handle);
     }
 }
