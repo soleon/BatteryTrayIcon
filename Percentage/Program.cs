58e75619bd90a7379917707e1c1dcaa1e4ab9d0b
Leon <leon.zhou@smokeball.com>
2020-02-21 00:04:19 +1100

    Faster and simpler DPI detection, better font retrieval and sizing, added tool tip and balloon notification.

--- Percentage/Program.cs
+++ Percentage/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Drawing;
 using System.Globalization;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows.Forms;
 using Microsoft.Win32;
 
@@ -15,13 +16,12 @@ namespace Percentage
             Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
             Application.EnableVisualStyles();
 
-            using var notifyIcon = new NotifyIcon {Visible = true};
+            using var notifyIcon = new NotifyIcon {Visible = true, BalloonTipIcon = ToolTipIcon.Info};
             var item = new ToolStripMenuItem("Exit");
             item.Click += (_, __) => Application.Exit();
             notifyIcon.ContextMenuStrip = new ContextMenuStrip {Items = {item}};
-
-            var dpi = new Control().DeviceDpi;
             Update();
+            notifyIcon.Click += (_, __) => notifyIcon.ShowBalloonTip(0);
 
             var timer = new Timer {Interval = 10000};
             timer.Tick += (_, __) => Update();
@@ -33,30 +33,74 @@ namespace Percentage
             {
                 var powerStatus = SystemInformation.PowerStatus;
                 var batteryChargeStatus = powerStatus.BatteryChargeStatus;
-                var isCharging =
-                    batteryChargeStatus.HasFlag(BatteryChargeStatus.Charging);
-                var isCritical =
-                    batteryChargeStatus.HasFlag(BatteryChargeStatus.Critical);
-                var isLow = batteryChargeStatus.HasFlag(BatteryChargeStatus.Low);
                 Brush brush;
-                if (isCharging)
+                var percent = (int) Math.Round(powerStatus.BatteryLifePercent * 100);
+                if (batteryChargeStatus.HasFlag(BatteryChargeStatus.Charging))
                 {
-                    brush = Brushes.RoyalBlue;
-                }
-                else if (isCritical)
-                {
-                    brush = Brushes.Red;
-                }
-                else if (isLow)
-                {
-                    brush = Brushes.OrangeRed;
+                    SetBrush();
+                    SetText(notifyIcon.BalloonTipTitle = "Charging", powerStatus.BatteryFullLifetime, " until fully charged");
                 }
                 else
                 {
-                    var isWin10LightTheme = 1.Equals(Registry.GetValue(
+                    if (batteryChargeStatus.HasFlag(BatteryChargeStatus.Critical))
+                    {
+                        brush = Brushes.Red;
+                    }
+                    else if (batteryChargeStatus.HasFlag(BatteryChargeStatus.Low))
+                    {
+                        brush = Brushes.OrangeRed;
+                    }
+                    else
+                    {
+                        SetBrush();
+                    }
+
+                    SetText(notifyIcon.BalloonTipTitle = "On battery", powerStatus.BatteryLifeRemaining, " remaining");
+                }
+
+                void SetText(string title, int seconds, string suffix)
+                {
+                    if (percent == 100)
+                    {
+                        notifyIcon.Text = title + Environment.NewLine + (notifyIcon.BalloonTipText = "Fully charged");
+                    }
+                    else
+                    {
+                        var builder = new StringBuilder();
+                        var duration = TimeSpan.FromSeconds(seconds);
+                        Append(duration.Hours, "hour");
+                        Append(duration.Minutes, "minute");
+                        builder.Append(suffix);
+                        notifyIcon.Text = title + Environment.NewLine + (notifyIcon.BalloonTipText = builder.ToString());
+
+                        void Append(int value, string unit)
+                        {
+                            if (value == 0)
+                            {
+                                return;
+                            }
+
+                            if (builder.Length > 0)
+                            {
+                                builder.Append(" ");
+                            }
+
+                            builder.Append(value + " " + unit);
+                            if (value > 1)
+                            {
+                                builder.Append("s");
+                            }
+                        }
+                    }
+                }
+
+                void SetBrush()
+                {
+                    brush = 1.Equals(Registry.GetValue(
                         @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize",
-                        "SystemUsesLightTheme", null));
-                    brush = isWin10LightTheme ? Brushes.Black : Brushes.White;
+                        "SystemUsesLightTheme", null))
+                        ? Brushes.Black
+                        : Brushes.White;
                 }
 
                 string text;
@@ -70,17 +114,18 @@ namespace Percentage
                 }
                 else
                 {
-                    text = Math.Round(powerStatus.BatteryLifePercent * 100).ToString(CultureInfo.CurrentCulture);
+                    text = percent.ToString(CultureInfo.CurrentCulture);
                 }
 
-                var font = new Font(SystemFonts.DefaultFont.FontFamily, SystemFonts.DefaultFont.Size * (dpi/96f));
                 int textWidth, textHeight;
+                Font font;
                 using (var bitmap = new Bitmap(1, 1))
                 {
                     using var graphics = Graphics.FromImage(bitmap);
-                    var size = graphics.MeasureString(text, font).ToSize();
-                    textWidth = size.Width;
-                    textHeight = size.Height;
+                    font = SystemInformation.GetMenuFontForDpi((int) graphics.DpiX);
+                    var size = graphics.MeasureString(text, font);
+                    textWidth = (int) Math.Round(size.Width);
+                    textHeight = (int) Math.Round(size.Height);
                 }
 
                 var iconDimension = Math.Max(textWidth, textHeight);
@@ -89,9 +134,8 @@ namespace Percentage
                 {
                     using (var graphics = Graphics.FromImage(bitmap))
                     {
-                        var halfIconDimension = iconDimension / 2;
-                        graphics.DrawString(text, font, brush, halfIconDimension - textWidth / 2,
-                            halfIconDimension - textHeight / 2);
+                        graphics.DrawString(text, font, brush, (iconDimension - textWidth) / 2f,
+                            (iconDimension - textHeight) / 2f);
                     }
 
                     var handle = bitmap.GetHicon();
